1ffe072c03d6e4ff1807f1cc2316435c67c7e4fb
eguonzy <60861214+eguonzy@users.noreply.github.com>
2020-07-11 19:16:45 +0100

    changed blue positioning

--- main.js
+++ main.js
@@ -9,10 +9,23 @@ let input = document.querySelector("#email");
 let em = document.querySelector("em");
 let sub = document.querySelector("#sub");
 let err = document.querySelector("#errorIcon");
+let red = document.querySelector(".redd");
+let red2 = document.querySelector(".redd2");
+let red3 = document.querySelector(".redd3");
 
 line1.addEventListener("click", (e) => {
-    console.log(line1.getAttribute("class"));
-    if (getComputedStyle(line1).borderBottomColor == "rgb(233, 227, 227)") {
+    if (window.innerWidth <= 500) {
+        console.log(window.innerWidth);
+        red.style.display = "block";
+        red2.style.display = "none";
+        red3.style.display = "none";
+        line1.style.color = "#242a45";
+        line2.style.color = "rgb(147, 144, 144)";
+        line3.style.color = "rgb(147, 144, 144)";
+        simple.style.display = "flex";
+        speedy.style.display = "none";
+        share.style.display = "none";
+    } else {
         line1.setAttribute("class", "dark1 hover");
         line2.setAttribute("class", "line2 hover");
         line3.setAttribute("class", "line3 hover");
@@ -23,21 +36,44 @@ line1.addEventListener("click", (e) => {
     }
 });
 line2.addEventListener("click", (e) => {
-    if (getComputedStyle(line2).borderBottomColor == "rgb(233, 227, 227)") {
+    if (window.innerWidth <= 500) {
+        red.style.display = "none";
+        red2.style.display = "block";
+        red3.style.display = "none";
+        line1.style.color = "rgb(147, 144, 144)";
+        line2.style.color = "#242a45";
+        line3.style.color = "rgb(147, 144, 144)";
+
+        simple.style.display = "none";
+        speedy.style.display = "flex";
+        share.style.display = "none";
+    } else {
         line1.setAttribute("class", "line1 hover");
         line2.setAttribute("class", "dark2 hover");
         line3.setAttribute("class", "line3 hover");
+
         simple.style.display = "none";
         speedy.style.display = "flex";
         share.style.display = "none";
     }
 });
 line3.addEventListener("click", (e) => {
-    if (getComputedStyle(line3).borderBottomColor == "rgb(233, 227, 227)") {
-        console.log("in");
+    if (window.innerWidth <= 500) {
+        red.style.display = "none";
+        red2.style.display = "none";
+        red3.style.display = "block";
+        line1.style.color = "rgb(147, 144, 144)";
+        line2.style.color = "rgb(147, 144, 144)";
+        line3.style.color = "#242a45";
+
+        simple.style.display = "none";
+        speedy.style.display = "none";
+        share.style.display = "flex";
+    } else {
         line1.setAttribute("class", "line1 hover");
         line2.setAttribute("class", "line2 hover");
         line3.setAttribute("class", "dark3 hover");
+
         simple.style.display = "none";
         speedy.style.display = "none";
         share.style.display = "flex";
